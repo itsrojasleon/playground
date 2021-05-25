2da03281090ba489ce92c2b7bc151d0587540d85
rojasleon <rojasleon.dev@gmail.com>
2021-05-24 19:55:56 -0500

    Please don't tell me that...

--- main.go
+++ main.go
@@ -74,6 +74,6 @@ func main() {
 	spa := spaHandler{staticPath: "client/build", indexPath: "index.html"}
 	r.PathPrefix("/").Handler(spa)
 
-	fmt.Println("Listening on port", port)
+	fmt.Println("Listening on port", ":"+port)
 	log.Fatal(http.ListenAndServe(port, r))
 }
